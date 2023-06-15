e7bcd44699f918ca929768063b7212c9df1449ea
Gang <ganglix@gmail.com>
2023-06-15 10:38:20 -0600

    delete path insert

--- mock_modules/module2.py
+++ mock_modules/module2.py
@@ -1,7 +1,3 @@
-# import sys
-# sys.path.insert(0, './')
-# from mock_modules import module1 as m1
-
 import module1 as m1
 def repeat_array_four_times(arr):
     """repeat_array_four_times return an array that repeats the input arr for four times
